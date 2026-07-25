29df1a7005b7d14d3f3dbf445524ff78a3704c7d
Mona1Tarek <https://github.com/Mona1Tarek>
2026-07-25 04:34:30 +0300

    Added llm config

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,11 +1,17 @@
+from typing import Optional
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     app_name: str = "AI Developer Coach"
     log_level: str = "INFO"
+    groq_api_key: str
+    llm_provider: Optional[str] = None
+    generation_model_name: Optional[str] = None
 
     model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
 
 
 settings = Settings()
+
