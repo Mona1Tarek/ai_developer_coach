d34f56c8cd1e1fb222b3e977b5f1a8ec5ef37fc7
Mona1Tarek <https://github.com/Mona1Tarek>
2026-08-05 19:22:03 +0300

    edits in the tutor folder to be more organized

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.routes.health import router as health_router
-from app.tutor.router import router as tutor_router
+from app.api.routes.tutor import router as tutor_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
